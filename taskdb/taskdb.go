47698739f58fbd83acbd100e5cb6706c333431cb
mdesson <michael.desson@gmail.com>
2020-03-08 11:52:48 -0400

    Finished AddTask

--- taskdb/taskdb.go
+++ taskdb/taskdb.go
@@ -1,6 +1,8 @@
 package taskdb
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"log"
 
 	"github.com/boltdb/bolt"
@@ -10,13 +12,13 @@ import (
 
 // Task Holds a todo list task
 type Task struct {
-	Desc []byte
+	Desc string
 	ID   int
-	done bool
+	Done bool
 }
 
 // Init Create a task database
-func Init() (*TaskDB, error) {
+func Init() (*bolt.DB, error) {
 	// create db
 	db, err := bolt.Open("tasks.db", 0600, nil)
 	if err != nil {
@@ -35,14 +37,37 @@ func Init() (*TaskDB, error) {
 // AddTask Creates a new task in the database
 func AddTask(db *bolt.DB, task string) {
 	err := db.Update(func(tx *bolt.Tx) error {
+		// Get bucket
 		b := tx.Bucket([]byte("Tasks"))
+		// Create task
+		task := Task{Desc: task, Done: false}
+
+		// Generate id from bucket
 		id, _ := b.NextSequence()
-		e := b.Put(id, task)
+
+		// Add to task as int
+		task.ID = int(id)
+
+		// Convert task to buffer
+		buf, e := json.Marshal(task)
+		if e != nil {
+			return e
+		}
+
+		// Store in db
+		e = b.Put(itob(task.ID), buf)
 		return e
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Convert int to byte array (width 8)
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
 }
 
 // func main() {
